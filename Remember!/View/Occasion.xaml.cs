7ba9283687c661cf55c2588370155c371a7e2739
Jesse Liberty <jesseliberty@gmail.com>
2015-12-30 16:51:06 -0500

    Cleaned up ui, fixed month display

--- Remember!/View/Occasion.xaml.cs
+++ Remember!/View/Occasion.xaml.cs
@@ -4,10 +4,43 @@ using System.Collections.Generic;
 using Xamarin.Forms;
 
 namespace Remember {
-   public partial class Occasion : ContentPage {
-      public Occasion() {
-         InitializeComponent();
-      }
-   }
+ 
+	public partial class Occasion : ContentPage {
+ 
+		private int updateId = 0;
+
+		public List<OccasionData> Occasions { get; set; }
+
+		public Occasion() {
+			InitializeComponent();
+		}
+
+		public void OnSaveClicked(object o, EventArgs e) {
+			OccasionData occasionData = new OccasionData();
+			occasionData.Name = OccasionEntry.Text;
+			occasionData.Date = OccasionDate.Date;
+			occasionData.Yearly = Yearly.IsToggled;
+			occasionData.NotificationDays = string.IsNullOrEmpty(Notification.Text) ? 0 : int.Parse(Notification.Text);
+			Clear();
+			App.Database.SaveOccasion(occasionData);
+			OccasionList.ItemsSource = App.Database.GetOccasions();
+		}
+
+		public void OnCancelClicked(object o, EventArgs e) {
+			Clear();
+		}
+
+		private void Clear() {
+			OccasionEntry.Text = string.Empty;
+			Notification.Text = string.Empty;
+			OccasionDate.Date = DateTime.Now;
+		}
+
+		protected override void OnAppearing() {
+			base.OnAppearing();
+			OccasionList.ItemsSource = App.Database.GetOccasions();
+
+		}
+	}
 }
 
